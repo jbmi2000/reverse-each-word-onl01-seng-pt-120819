3da5b8dd136b731c4a8113605a62ef73fc524804
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-19 07:15:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ sentence1="Hello, there and how are you?"
 def reverse_each_word(sentence1)
   new_array = []
   reversed_words = []
-  new_array << sentence1.split('')
+  new_array << sentence1.split
   new_array.collect {|word| reversed_words << word.reverse}
 
   binding.pry
